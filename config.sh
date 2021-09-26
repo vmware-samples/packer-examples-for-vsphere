b45c8e6c81601f5e8c836c8e226a1ca50af4c200
Ryan Johnson <johnsonryan@vmware.com>
2021-09-26 18:57:45 -0400

    Update config.sh
    Copy the `*.pkrvars.hcl.example` files and change extension to `pkrvars.hcl` in `$CONFIG_PATH`.

--- config.sh
+++ config.sh
@@ -15,6 +15,22 @@ SCRIPT_PATH=$(realpath "$(dirname "$(follow_link "$0")")")
 CONFIG_PATH=${1:-${SCRIPT_PATH}/config}
 
 mkdir -p "$CONFIG_PATH"
+### Copy the example input variables.
 echo
-echo "> Copy default builds configuration"
-cp -av "$SCRIPT_PATH"/builds/*.pkrvars.hcl "$CONFIG_PATH"
+echo "> Copying the example input variables ..."
+cp -av "$SCRIPT_PATH"/builds/*.pkrvars.hcl.example "$CONFIG_PATH"
+
+### Rename the example input variables.
+echo
+echo "> Renaming the example input variables ..."
+srcext=".pkrvars.hcl.example"
+dstext=".pkrvars.hcl"
+
+for f in "$CONFIG_PATH"/*"${srcext}"; do
+  bname="${f%${srcext}}"
+  echo "${bname}{${srcext} → ${dstext}}"
+  mv "${f}" "${bname}${dstext}"
+done
+
+echo
+echo "> Done."

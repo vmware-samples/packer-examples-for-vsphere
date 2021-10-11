vmware-samples/packer-examples-for-vsphere

commit 6f538fea8414984dd0620427283a1e7fdbc64e10

Update `manifest` post-provisioner `custom_data` (#115)
- Updates `local.path_manifest` to `local.manifest_path`.
- Adds `local.manifest_date` to be used as the name of the manifest JSON.
- Adds `custom_data` to the `manifest` post-provisioner for each build.

builds/windows/windows-10/windows.pkr.hcl
@@ -27,7 +27,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
 }
 
 //  BLOCK: source
@@ -163,7 +164,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/windows/windows-server-2016/windows-server.pkr.hcl
@@ -27,7 +27,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
 }
 
 //  BLOCK: source
@@ -424,7 +425,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_member}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/windows/windows-server-2019/windows-server.pkr.hcl
@@ -27,7 +27,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
 }
 
 //  BLOCK: source
@@ -425,7 +426,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_member}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/windows/windows-server-2022/windows-server.pkr.hcl
@@ -27,7 +27,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
 }
 
 //  BLOCK: source
@@ -425,7 +426,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_member}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/linux/almalinux-8/linux-almalinux.pkr.hcl
@@ -21,7 +21,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
   data_source_content = {
     "/ks.cfg" = templatefile("${abspath(path.root)}/data/ks.pkrtpl.hcl", {
       build_username           = var.build_username
@@ -144,7 +145,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_vendor}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/linux/centos-linux-7/linux-centos-linux.pkr.hcl
@@ -21,7 +21,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
   data_source_content = {
     "/ks.cfg" = templatefile("${abspath(path.root)}/data/ks.pkrtpl.hcl", {
       build_username           = var.build_username
@@ -144,7 +145,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_vendor}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/linux/centos-linux-8/linux-centos-linux.pkr.hcl
@@ -21,7 +21,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
   data_source_content = {
     "/ks.cfg" = templatefile("${abspath(path.root)}/data/ks.pkrtpl.hcl", {
       build_username           = var.build_username
@@ -144,7 +145,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_vendor}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

builds/linux/centos-stream-8/linux-centos-stream.pkr.hcl
@@ -21,7 +21,8 @@ packer {
 
 locals {
   buildtime     = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp())
-  path_manifest = "${path.cwd}/manifests/"
+  manifest_date = formatdate("YYYY-MM-DD hh:mm:ss", timestamp())
+  manifest_path = "${path.cwd}/manifests/"
   data_source_content = {
     "/ks.cfg" = templatefile("${abspath(path.root)}/data/ks.pkrtpl.hcl", {
       build_username           = var.build_username
@@ -144,7 +145,29 @@ build {
   }
 
   post-processor "manifest" {
-    output     = "${local.path_manifest}${local.buildtime} ${var.vm_guest_os_family}-${var.vm_guest_os_vendor}.json"
-    strip_path = false
+    output     = "${local.manifest_path}${local.manifest_date}.json"
+    strip_path = true
+    strip_time = true
+    custom_data = {
+      ansible_username         = var.ansible_username
+      build_username           = var.build_username
+      buildtime                = local.buildtime
+      common_data_source       = var.common_data_source
+      common_vm_version        = var.common_vm_version
+      vm_cpu_cores             = var.vm_cpu_cores
+      vm_cpu_sockets           = var.vm_cpu_sockets
+      vm_disk_size             = var.vm_disk_size
+      vm_disk_thin_provisioned = var.vm_disk_thin_provisioned
+      vm_firmware              = var.vm_firmware
+      vm_guest_os_type         = var.vm_guest_os_type
+      vm_mem_size              = var.vm_mem_size
+      vm_network_card          = var.vm_network_card
+      vsphere_cluster          = var.vsphere_cluster
+      vsphere_datacenter       = var.vsphere_datacenter
+      vsphere_datastore        = var.vsphere_datastore
+      vsphere_endpoint         = var.vsphere_endpoint
+      vsphere_folder           = var.vsphere_folder
+      vsphere_iso_path         = "[${var.common_iso_datastore}] ${var.iso_path}/${var.iso_file}"
+    }
   }
 }

commit 687f19b80e957ceb7f3a2f54aecc542e7918dfea

Fix `manifest` issue in Windows builds
Fixes issue in the `manifest` for Windows builds introduced in #115.

builds/windows/windows-10/windows.pkr.hcl
@@ -168,7 +168,6 @@ build {
     strip_path = true
     strip_time = true
     custom_data = {
-      ansible_username         = var.ansible_username
       build_username           = var.build_username
       buildtime                = local.buildtime
       common_data_source       = var.common_data_source

builds/windows/windows-server-2016/windows-server.pkr.hcl
@@ -429,7 +429,6 @@ build {
     strip_path = true
     strip_time = true
     custom_data = {
-      ansible_username         = var.ansible_username
       build_username           = var.build_username
       buildtime                = local.buildtime
       common_data_source       = var.common_data_source

builds/windows/windows-server-2019/windows-server.pkr.hcl
@@ -430,7 +430,6 @@ build {
     strip_path = true
     strip_time = true
     custom_data = {
-      ansible_username         = var.ansible_username
       build_username           = var.build_username
       buildtime                = local.buildtime
       common_data_source       = var.common_data_source

builds/windows/windows-server-2022/windows-server.pkr.hcl
@@ -430,7 +430,6 @@ build {
     strip_path = true
     strip_time = true
     custom_data = {
-      ansible_username         = var.ansible_username
       build_username           = var.build_username
       buildtime                = local.buildtime
       common_data_source       = var.common_data_source
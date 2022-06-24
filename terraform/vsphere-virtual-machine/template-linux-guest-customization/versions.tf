82b99be32bbe1d23ed0a3eb8c496fa6807b7e6b3
Ryan Johnson <johnsonryan@vmware.com>
2022-06-24 10:51:10 -0400

    chore: bump to terraform v1.2.3 and hashicorp/vsphere v2.2.0
    * Updates `required_versions` for `terraform` to `>= v1.2.3`.
    * Updates `required_versions` for `hashicorp/vsphere` to `>= v2.2.0`.
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- terraform/vsphere-virtual-machine/template-linux-guest-customization/versions.tf
+++ terraform/vsphere-virtual-machine/template-linux-guest-customization/versions.tf
@@ -6,8 +6,8 @@ terraform {
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = ">= 2.1.1"
+      version = ">= 2.2.0"
     }
   }
-  required_version = ">= 1.2.1"
+  required_version = ">= 1.2.3"
 }

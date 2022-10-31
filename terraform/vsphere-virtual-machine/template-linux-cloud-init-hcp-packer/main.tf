f537763688f7683974d8ec4e32c18ee00c240e4c
Ryan Johnson <johnsonryan@vmware.com>
2022-10-31 16:40:56 -0400

    fix: update terraform deprecated index (#321)
    Updates Terraform examples to ensure list items are accessed using square brackets.
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- terraform/vsphere-virtual-machine/template-linux-cloud-init-hcp-packer/main.tf
+++ terraform/vsphere-virtual-machine/template-linux-cloud-init-hcp-packer/main.tf
@@ -56,9 +56,9 @@ resource "vsphere_virtual_machine" "vm" {
   }
   disk {
     label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks.0.size
-    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    size             = data.vsphere_virtual_machine.template.disks[0].size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks[0].eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id

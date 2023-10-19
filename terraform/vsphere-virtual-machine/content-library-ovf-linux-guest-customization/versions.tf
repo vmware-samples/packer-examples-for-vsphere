613709506e443b9b6285da197fcdabc68ed6bceb
Ryan Johnson <johnsonryan@vmware.com>
2023-10-19 15:21:50 -0400

    chore(deps): bump hashcorp/vsphere from v2.4.0 to v2.5.1 (#758)
    Bumps Terraform Provider for VMware vSphere (`hashicorp/vsphere`) from v2.4.0 to v2.5.1.
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- terraform/vsphere-virtual-machine/content-library-ovf-linux-guest-customization/versions.tf
+++ terraform/vsphere-virtual-machine/content-library-ovf-linux-guest-customization/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = ">= 2.4.0"
+      version = ">= 2.5.1"
     }
   }
   required_version = ">= 1.6.0"

37addc47229945e3ba99b7af4304bd147e1eaf2b
Ryan Johnson <ryan.johnson@broadcom.com>
2024-01-26 15:56:10 -0500

    chore(deps): bumperraform-provider-hcp from v0.74.1 to v0.81.0 (#831)
    Bumps Terraform Provider for HCP (`hashicorpn/terraform-provider-hcp`) from  v0.74.1 to v0.81.0.
    
    Signed-off-by: Ryan Johnson <ryan.johnson@broadcom.com>

--- terraform/vsphere-virtual-machine/content-library-template-linux-guest-customization-hcp-packer/versions.tf
+++ terraform/vsphere-virtual-machine/content-library-template-linux-guest-customization-hcp-packer/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     hcp = {
       source  = "hashicorp/hcp"
-      version = "~> 0.74.1"
+      version = "~> 0.81.0"
     }
     vsphere = {
       source  = "hashicorp/vsphere"
